92ab969e6f593de54bffaca6d4d73c6388574d30
Priyam Mohanty <priyam.mohanty@berkeley.edu>
2019-07-30 22:46:23 -0700

    1

---
+++ 101.symmetric-tree.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=101 lang=java
+ *
+ * [101] Symmetric Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    public boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return check(left.left, right.right) && check(left.right, right.left);
+    }
+}
+
---
+++ 48.rotate-image.java
@@ -0,0 +1,11 @@
+/*
+ * @lc app=leetcode id=48 lang=java
+ *
+ * [48] Rotate Image
+ */
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+    }
+}
+
